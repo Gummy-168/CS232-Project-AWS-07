47a626a159195c7f3ac3baedf41cb15edc1dea26
สิริยากร พูนสินโภคทรัพย์ <168855741+ShawtyBob@users.noreply.github.com>
2026-05-03 03:07:15 +0700

    add analystics page

--- frontend/app/professor/layout.tsx
+++ frontend/app/professor/layout.tsx
@@ -1,14 +1,16 @@
 import ProfessorSidebar from "../components/profSidebar";
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function ProfessorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex">
       <ProfessorSidebar />
-      <div className="flex-1 ml-64 flex flex-col min-h-screen">
-        <main className="flex-1 overflow-y-auto pt-16">
-          {children}
-        </main>
-      </div>
+      <main className="flex-1 bg-slate-50 min-h-screen">
+        {children}
+      </main>
     </div>
   );
 }
